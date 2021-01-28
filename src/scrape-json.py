b08406e75df47b574025861bca89c5a2f0ce7959
t.phil <thomas@tphil.nl>
2021-01-28 12:22:20 +0100

    Pulling dose deliveries from json instead of svg now

---
+++ src/scrape-json.py
@@ -0,0 +1,15 @@
+import requests
+
+
+data = requests.get('https://coronadashboard.rijksoverheid.nl/json/NL.json').json()
+df = pd.DataFrame(data['vaccine_delivery']['values'])
+for col in df.columns:
+    if 'date' in col:
+        df[col] = pd.to_datetime(df[col], unit='s')
+    else:
+        df[col] = df[col].astype(int)
+        
+#df = df.set_index('date_start_unix')
+df['year-week'] = df['date_start_unix'].dt.strftime('%G-%V')
+df = df.set_index('year-week')
+df.to_csv('vaccine-dose-deliveries-by-manufacturer.csv')
