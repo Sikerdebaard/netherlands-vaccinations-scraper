1d57ee465cee1e3a9c169b67b2d50fd1667f931f
T Phil <thomas@tphil.nl>
2021-02-20 14:18:14 +0100

    Improved NAN handling in augmented cumulative doses administered

--- src/owid-augment.py
+++ src/owid-augment.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 df_owid = pd.read_csv('https://raw.githubusercontent.com/owid/covid-19-data/79bc3c1a690454579ff275be34a347ab6125fb04/public/data/vaccinations/country_data/Netherlands.csv', index_col=['date'])
 df_owid.index = pd.to_datetime(df_owid.index)
@@ -21,7 +22,9 @@ df_merged = df_merged[['reported', 'estimated', 'source']]
 
 df_merged.to_csv('augmented/doses_administered_raw.csv')
 
-df_diff = df_merged['reported'].diff()
-df_cumulative = df_merged[~(df_merged.index.isin(df_diff[df_diff < 0].index))]
-
+for col in ['reported', 'estimated']:
+    df_diff = df_merged[col].diff()
+    df_merged.loc[df_merged.index.isin(df_diff[df_diff < 0].index), col] = np.nan
+    df_merged[col] = df_merged[col].astype(pd.Int64Dtype())
+                                   
 df_merged.to_csv('augmented/doses_administered_cumulative.csv')
