260ca9078ff846614949441aece70cce712a0d18
T Phil <thomas@tphil.nl>
2021-02-24 15:56:00 +0100

    Small update to scraper

--- src/scrape-doses-administered.py
+++ src/scrape-doses-administered.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import requests
+from pathlib import Path
 
 
 data = requests.get('https://coronadashboard.rijksoverheid.nl/json/NL.json')
@@ -8,10 +9,22 @@ data.raise_for_status()
 for k, v in data.json().items():
     if 'vaccine_administered' in k:
         df = pd.DataFrame(data.json()[k]['values'])
+        out_file = Path(f'{k}.csv')
         if 'date_unix' in df.columns:
             df = df.set_index('date_unix').astype(pd.Int64Dtype())
             df.index = pd.to_datetime(df.index, unit='s')
             df.index.rename('date', inplace=True)
             df.sort_index(inplace=True)
 
-        df.to_csv(f'{k}.csv')
+            if out_file.exists():
+                df_org = pd.read_csv(out_file, index_col=0)
+                df_org.index = pd.to_datetime(df_org.index)
+
+                for idx, row in df.iterrows():
+                    for col in df.columns:
+                        df_org.at[idx, col] = row[col]
+
+                df = df_org
+
+
+        df.astype(pd.Int64Dtype()).to_csv(out_file)
